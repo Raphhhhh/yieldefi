5d4b62852a8450e9c18b94e004f2163a77810fa0
Raph <raph_prat2@hotmail.com>
2021-04-30 18:51:05 +0200

    stakedao pool btc

--- src/store/stakedao.js
+++ src/store/stakedao.js
@@ -2,27 +2,26 @@ import Vue from 'vue'
 import { getSimpleVault } from '~/helpers/ethersHelper'
 
 const decimals = 18
-const stakingContract = '0xfEA5E213bbD81A8a94D0E1eDB09dBD7CEab61e1c'
+
 const stakingContractAbi = [
   'function userInfo(uint256,address) view returns (uint, uint)',
 ]
-const passiveEurContract = '0xCD6997334867728ba14d7922f72c893fcee70e84'
-const passiveEurContractAbi = [
+
+const stakeDaoContractAbi = [
   'function getPricePerFullShare() view returns (uint)',
 ]
-const curveCRVEursContract = '0x0Ce6a5fF5217e38315f87032CF90686C96627CAA'
-const curveCRVEursContractAbi = [
+
+const curveContractAbi = [
   'function get_virtual_price() view external returns (uint)',
 ]
 
-const passive3PoolContract = '0xB17640796e4c27a39AF51887aff3F8DC0daF9567'
-const passive3PoolContractAbi = [
-  'function getPricePerFullShare() view returns (uint)',
-]
+const stakingContract = '0xfEA5E213bbD81A8a94D0E1eDB09dBD7CEab61e1c'
+const eurContract = '0xCD6997334867728ba14d7922f72c893fcee70e84'
+const threePoolContract = '0xB17640796e4c27a39AF51887aff3F8DC0daF9567'
+const btcContract = '0x24129B935AfF071c4f0554882C0D9573F4975fEd'
+const curveCRVEursContract = '0x0Ce6a5fF5217e38315f87032CF90686C96627CAA'
 const curveCRV3PoolContract = '0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7'
-const curveCRV3PoolContractAbi = [
-  'function get_virtual_price() view external returns (uint)',
-]
+const curveCRVBtcContract = '0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714'
 
 export const state = () => ({
   userVaults: [],
@@ -60,18 +59,8 @@ export const actions = {
         decimals,
       ],
       [
-        [
-          passiveEurContract,
-          passiveEurContractAbi,
-          'getPricePerFullShare',
-          decimals,
-        ],
-        [
-          curveCRVEursContract,
-          curveCRVEursContractAbi,
-          'get_virtual_price',
-          decimals,
-        ],
+        [eurContract, stakeDaoContractAbi, 'getPricePerFullShare', decimals],
+        [curveCRVEursContract, curveContractAbi, 'get_virtual_price', decimals],
       ]
     )
 
@@ -82,24 +71,39 @@ export const actions = {
         stakingContract,
         stakingContractAbi,
         'userInfo',
-        [0, ctx.rootState.ethers.address],
+        [1, ctx.rootState.ethers.address],
         decimals,
       ],
       [
         [
-          passive3PoolContract,
-          passive3PoolContractAbi,
+          threePoolContract,
+          stakeDaoContractAbi,
           'getPricePerFullShare',
           decimals,
         ],
         [
           curveCRV3PoolContract,
-          curveCRV3PoolContractAbi,
+          curveContractAbi,
           'get_virtual_price',
           decimals,
         ],
       ]
     )
     ctx.commit('pushUserVault', { ...requests3Pool, name: 'sd3Pools' })
+
+    const requestsBtc = await getSimpleVault(
+      [
+        stakingContract,
+        stakingContractAbi,
+        'userInfo',
+        [3, ctx.rootState.ethers.address],
+        decimals,
+      ],
+      [
+        [btcContract, stakeDaoContractAbi, 'getPricePerFullShare', decimals],
+        [curveCRVBtcContract, curveContractAbi, 'get_virtual_price', decimals],
+      ]
+    )
+    ctx.commit('pushUserVault', { ...requestsBtc, name: 'sdcrvRenWSBTC' })
   },
 }
